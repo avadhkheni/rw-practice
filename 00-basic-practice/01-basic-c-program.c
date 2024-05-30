9e251ca3fce9a05fc98e07515a65d2694013dc63
avadhkheni <avadhkheni29@gmail.com>
2024-05-30 11:28:42 +0530

    user-1 changes are done

--- 00-basic-practice/01-basic-c-program.c
+++ 00-basic-practice/01-basic-c-program.c
@@ -4,6 +4,7 @@ int main(){
 
     printf("Hello wellcome to vs code");
     printf("wellcome to jaimin sir lectires");
+    printf("we are in user-1 branch");
 
     return 0;
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 -- today i have learned git basics
 -- today revision lecture for GIT and GIT HUB
+-- user one branch
